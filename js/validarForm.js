27a79f1dc7eb90a1f42c508e5f40ff3244a4683a
Pepe de Soto <pepesoto240@gmail.com>
2020-12-02 19:53:02 +0100

    Error en validadForm.js

---
+++ js/validarForm.js
@@ -0,0 +1,12 @@
+
+let usu = document.getElementById("usuario");
+
+// const formulario = document.getElementById('formulario');
+// const inputs = document.querySelectorAll("#formulario input")
+
+// const expresiones = {
+//     usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
+//     nombre:  /^[a-zA-ZÁ-ÿ\s]{1,40}$/,
+//     password:  /^.{4,12}$/,
+//     correo: /^[a-zA-Z0-9_.+-]+@[aa-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+// }
