235743d6163b78b944f4ca88f05f2b16e8a1139a
Pepe de Soto <pepesoto240@gmail.com>
2020-12-03 10:49:01 +0100

    Solucionado formulario/falta acabarlas validaciones

--- js/validarForm.js
+++ js/validarForm.js
@@ -1,12 +1,38 @@
 
-let usu = document.getElementById("usuario");
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll("#formulario input")
 
-// const formulario = document.getElementById('formulario');
-// const inputs = document.querySelectorAll("#formulario input")
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
+    nombre:  /^[a-zA-ZÁ-ÿ\s]{1,40}$/,
+    password:  /^.{4,12}$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[aa-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
 
-// const expresiones = {
-//     usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
-//     nombre:  /^[a-zA-ZÁ-ÿ\s]{1,40}$/,
-//     password:  /^.{4,12}$/,
-//     correo: /^[a-zA-Z0-9_.+-]+@[aa-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-// }
+const validarFormulario = (e)=>{
+    switch(e.target.name){
+        case "usuario":
+            if(expresiones.usuario.test(e.target.value)){
+
+            }else{
+                document.getElementById("usuario");
+            }
+        break;
+        case "nombre" || "materia":
+                
+        break;
+        case "email":
+                
+        break;
+    }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener("keyup",validarFormulario);
+    input.addEventListener("blur",validarFormulario);
+});
+
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+});
